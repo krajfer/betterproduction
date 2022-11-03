b1223d5fc629bc1db684d0af7bfc9b333de78cc8
krajfer <mrkrajfer@gmail.com>
2022-11-03 22:30:39 +0100

    there is progresssssssss

---
+++ better/Models/CalculatorContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace better.Models
+{
+    public class CalculatorContext: DbContext
+    {
+        public CalculatorContext(): base("name=ConnectionString")
+        {
+
+        }
+
+        public DbSet<SearchHistory> SearchHistory { get; set; }
+        public DbSet<Module> Module { get; set; }
+        public DbSet<City> City { get; set; }
+
+    }
+}
--- better/Models/SearchHistory.cs
+++ better/Models/SearchHistory.cs
@@ -8,8 +8,12 @@ namespace better.Models
     public class SearchHistory
     {
         public int Id { get; set; }
-        public string DateOfSearch { get; set; }
         public int CityId { get; set; }
-        public int ModuleId { get; set; }
+        public string ModuleName1 { get; set; }
+        public string ModuleName2 { get; set; }
+        public string ModuleName3 { get; set; }
+        public string ModuleName4 { get; set; }
+        public double ProductionCost { get; set; }
+
     }
 }
