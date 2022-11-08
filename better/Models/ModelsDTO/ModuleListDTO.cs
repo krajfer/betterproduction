c65a327a0edfe3c626cf44550b8217e3d5f5f580
krajfer <mrkrajfer@gmail.com>
2022-11-08 22:16:46 +0100

    Początkowe Modele DTO odjebane :3

---
+++ better/Models/ModelsDTO/ModuleListDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace better.Models.ModelsDTO
+{
+    public class ModuleListDTO
+    {
+        public List<string> ModuleList { get; set; }
+    }
+}
