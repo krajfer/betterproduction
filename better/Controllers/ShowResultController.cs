1df7901b52f923afbdb07b1ed8142551541ffcf6
krajfer <mrkrajfer@gmail.com>
2022-11-13 23:26:32 +0100

    Dodano kontrollery teraz trzeba je uzupełnić co więcej sprawdzić jak poprawnie korzystać z Global.asax coś tam jest chyba źle

---
+++ better/Controllers/ShowResultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace better.Controllers
+{
+    public class ShowResultController : Controller
+    {
+        // GET: ShowResult
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
